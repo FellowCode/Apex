24cedf959ec7b946e22b76827d3da504d099be61
FellowCode <31797522+FellowCode@users.noreply.github.com>
2019-04-13 02:04:58 +1000

    update

--- Service/helper.py
+++ Service/helper.py
@@ -3,7 +3,9 @@ def get_service_verbose_name(service_id):
         service_type_verbose = 'Выездная проверка автомобиля экспертом'
     elif service_id == '2':
         service_type_verbose = 'Эксперт на день'
-    else:
+    elif service_id == '3':
         service_type_verbose = 'Подбор автомобиля под ключ'
+    else:
+        service_type_verbose = 'Укажите услугу в примечании'
 
     return service_type_verbose
